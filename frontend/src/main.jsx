b598df244386a7337e9ed39704131ed97fd8697d
Curtis Aliwah <aliwahcurtis5@gmail.com>
2025-02-20 14:15:10 +0400

    feat: Implemented react routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import { createBrowserRouter, RouterProvider } from 'react-router'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+import Homepage from "./pages/Homepage/Homepage.jsx";
+import Feedback from "./pages/Feedback/Feedback.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/feedback",
+    element: <Feedback />,
+  },
+]);
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
