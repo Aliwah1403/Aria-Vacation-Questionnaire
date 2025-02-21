dc010e0c1a1693aa8074275d43b6a8d47c338406
Curtis Aliwah <aliwahcurtis5@gmail.com>
2025-02-21 16:27:12 +0400

    feat: Add aria logo

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -1,11 +1,14 @@
 import StayDetailsForm from "./stay-details-form";
+import AriaLogo from "@/assets/AriaLogo.png";
 
 const Homepage = () => {
   return (
     <div className="min-h-screen bg-white flex items-center justify-center">
       <div className="w-full max-w-xl p-6 space-y-8">
+        <img src={AriaLogo} className="mx-auto" width={300} height={120} />
+
         <div className="space-y-2 text-center">
-          <h1 className="text-3xl font-bold">Vacation Feedback</h1>
+          {/* <h1 className="text-3xl font-bold">Vacation Feedback</h1> */}
           <p className="text-gray-500">
             Please provide your stay details before proceeding.
           </p>
