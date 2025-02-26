d3f33c09008e8a56f8e8246eb561a9a0fa31120e
Curtis Aliwah <aliwahcurtis5@gmail.com>
2025-02-26 17:53:59 +0400

    refactor: Moved the questions into their own file

---
+++ frontend/src/pages/Feedback/Questions/Questions.js
@@ -0,0 +1,57 @@
+export const questions = [
+  {
+    id: 1,
+    text: "How efficient and hassle-free was the reservation and booking process?",
+    type: "emoji",
+  },
+  {
+    id: 2,
+    text: "How knowledgeable and helpful were the member services personnel in assisting with your booking?",
+    type: "emoji",
+  },
+  {
+    id: 3,
+    text: "How smoothly and quickly did the check-in and check-out process go?",
+    type: "emoji",
+  },
+  {
+    id: 4,
+    text: "How friendly and supportive were the resort staff during your stay?",
+    type: "emoji",
+  },
+  {
+    id: 5,
+    text: "How well-maintained and clean were the outdoor spaces, shared areas, buildings and facilities?",
+    type: "emoji",
+  },
+  {
+    id: 6,
+    text: "How would you rate the view and overall location of your accommodation?",
+    type: "emoji",
+  },
+  {
+    id: 7,
+    text: "Was the size of your accommodation comfortable and sufficient for your needs?",
+    type: "emoji",
+  },
+  {
+    id: 8,
+    text: "How well-maintained were the appliances, furniture, and overall condition of your accommodation?",
+    type: "emoji",
+  },
+  {
+    id: 9,
+    text: "How clean and ready was your accommodation upon arrival?",
+    type: "emoji",
+  },
+  {
+    id: 10,
+    text: "How effectively did the resort implement measures to ensure your health and safety during your stay?",
+    type: "emoji",
+  },
+  {
+    id: 11,
+    text: "Do you have any additional comments or suggestions regarding your experience?",
+    type: "text",
+  },
+];
