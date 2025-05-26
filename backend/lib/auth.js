aa7584f11a2fdb1313278e55c7144a1bb2bbb149
Curtis Aliwah <aliwahcurtis5@gmail.com>
2025-05-26 12:14:06 +0400

    refactor(auth): Changed some wording on password reset email

--- backend/lib/auth.js
+++ backend/lib/auth.js
@@ -26,49 +26,156 @@ export const auth = betterAuth({
         // text: `Click the link to reset your password: ${url}`,
         html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html dir="ltr" lang="en">
-
   <head>
     <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
-    <meta name="x-apple-disable-message-reformatting" /><!--$-->
+    <meta name="x-apple-disable-message-reformatting" />
+    <!--$-->
     <style>
       @font-face {
-        font-family: 'Inter';
+        font-family: "Poppins";
         font-style: normal;
         font-weight: 400;
-        mso-font-alt: 'Helvetica';
-        src: url(https://fonts.googleapis.com/css2?family=Inter&display=swap) format('woff2');
+        mso-font-alt: "Helvetica";
+        src: url(https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap)
+          format("woff2");
       }
 
       * {
-        font-family: 'Inter', Helvetica;
+        font-family: "Poppins", Helvetica;
       }
     </style>
   </head>
 
-  <body style="margin:0;margin-left:12px;margin-right:12px;background-color:#ffffff">
-    <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="max-width:37.5em;margin-left:auto;margin-right:auto;box-sizing:border-box;padding-top:1rem;padding-bottom:1rem;height:100vh">
+  <body
+    style="
+      margin: 0;
+      margin-left: 12px;
+      margin-right: 12px;
+      background-color: #ffffff;
+    "
+  >
+    <table
+      align="center"
+      width="100%"
+      border="0"
+      cellpadding="0"
+      cellspacing="0"
+      role="presentation"
+      style="
+        max-width: 37.5em;
+        margin-left: auto;
+        margin-right: auto;
+        box-sizing: border-box;
+        padding-top: 0.5rem;
+        padding-bottom: 0.5rem;
+        height: 100vh;
+      "
+    >
       <tbody>
-        <tr style="width:100%">
+        <tr style="width: 100%">
           <td>
-            <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="text-align:center">
+            <table
+              align="center"
+              width="100%"
+              border="0"
+              cellpadding="0"
+              cellspacing="0"
+              role="presentation"
+              style="text-align: center"
+            >
               <tbody>
                 <tr>
                   <td>
-                    <h1 style="font-size:24px;font-weight:600;line-height:32px;color:rgb(17,24,39)">
+                    <h1
+                      style="
+                        font-size: 24px;
+                        font-weight: 600;
+                        line-height: 32px;
+                        color: rgb(17, 24, 39);
+                      "
+                    >
                       Reset Your Password
                     </h1>
-                    <p style="font-size:16px;line-height:24px;color:rgb(107,114,128);margin-top:16px;margin:16px 0">
-                      You are receiving this email because we received a password reset
-                      request for your account.
+                    <p
+                      style="
+                        font-size: 16px;
+                        line-height: 24px;
+                        color: rgb(107, 114, 128);
+                        margin-top: 16px;
+                        margin: 16px 0;
+                      "
+                    >
+                      You are receiving this email because we received a
+                      password reset request for your account.
                     </p>
-                    <a href=${url} style="background-color:rgb(79,70,229);color:rgb(255,255,255);font-weight:600;border-radius:8px;padding-left:20px;padding-right:20px;padding-top:12px;padding-bottom:12px;margin-top:24px;line-height:100%;text-decoration:none;display:inline-block;max-width:100%;mso-padding-alt:0px;padding:12px 20px 12px 20px" target="_blank"><span><!--[if mso]><i style="mso-font-width:500%;mso-text-raise:18" hidden>&#8202;&#8202;</i><![endif]--></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:9px">
-      Reset Password
-    </span><span><!--[if mso]><i style="mso-font-width:500%" hidden>&#8202;&#8202;&#8203;</i><![endif]--></span></a>
-                    <p style="font-size:16px;line-height:24px;color:rgb(107,114,128);margin-top:16px;margin:16px 0">
+                    <a
+                      href="${url}"
+                      style="
+                        background-color: #4abec6;
+                        color: rgb(255, 255, 255);
+                        font-weight: 600;
+                        border-radius: 8px;
+                        padding-left: 20px;
+                        padding-right: 20px;
+                        padding-top: 12px;
+                        padding-bottom: 12px;
+                        margin-top: 24px;
+                        line-height: 100%;
+                        text-decoration: none;
+                        display: inline-block;
+                        max-width: 100%;
+                        mso-padding-alt: 0px;
+                        padding: 12px 20px 12px 20px;
+                      "
+                      target="_blank"
+                      ><span
+                        ><!--[if mso
+                          ]><i
+                            style="mso-font-width: 500%; mso-text-raise: 18"
+                            hidden
+                            >&#8202;&#8202;</i
+                          ><!
+                        [endif]--></span
+                      ><span
+                        style="
+                          max-width: 100%;
+                          display: inline-block;
+                          line-height: 120%;
+                          mso-padding-alt: 0px;
+                          mso-text-raise: 9px;
+                        "
+                      >
+                        Reset Password </span
+                      ><span
+                        ><!--[if mso
+                          ]><i style="mso-font-width: 500%" hidden
+                            >&#8202;&#8202;&#8203;</i
+                          ><!
+                        [endif]--></span
+                      ></a
+                    >
+                    <p
+                      style="
+                        font-size: 16px;
+                        line-height: 24px;
+                        color: rgb(107, 114, 128);
+                        margin-top: 16px;
+                        margin: 16px 0;
+                      "
+                    >
                       This password reset link will expire in 1 hour.
                     </p>
-                    <p style="font-size:16px;line-height:24px;color:rgb(107,114,128);margin-top:16px;margin:16px 0">
-                      If you did not request a password reset, no further action is required.
+                    <p
+                      style="
+                        font-size: 16px;
+                        line-height: 24px;
+                        color: rgb(107, 114, 128);
+                        margin-top: 16px;
+                        margin: 16px 0;
+                      "
+                    >
+                      If you did not request a password reset, no further action
+                      is required.
                     </p>
                   </td>
                 </tr>
@@ -77,13 +184,14 @@ export const auth = betterAuth({
           </td>
         </tr>
       </tbody>
-    </table><!--/$-->
+    </table>
+    <!--/$-->
   </body>
-
-</html>`,
+</html>
+`,
       });
     },
-    resetPasswordTokenExpiresIn: 3600, // 1 hour 
+    resetPasswordTokenExpiresIn: 3600, // 1 hour
 
     // disableSignUp: true,
   },
