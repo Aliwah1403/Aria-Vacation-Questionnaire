4b72aa416246d5168b89be0027918e6e384c0d88
Curtis Aliwah <aliwahcurtis5@gmail.com>
2025-07-12 10:37:05 +0400

    feat: Created feature to send test emails whenever user is creating or editing an email template so they can get a feel for how it will actually look

--- frontend/src/api/emailSend.js
+++ frontend/src/api/emailSend.js
@@ -1,11 +1,18 @@
 import apiClient from "@/lib/axios";
 
 export const emailSendApi = {
-    sendFeedbackEmail: async (emailData) => {
-        const { data } = await apiClient.post(
-          "/api/v1/email-send/send-feedback",
-          emailData
-        );
-        return data;
-    },
-}
+  sendFeedbackEmail: async (emailData) => {
+    const { data } = await apiClient.post(
+      "/api/v1/email-send/send-feedback",
+      emailData
+    );
+    return data;
+  },
+  sendTestEmail: async (emailData) => {
+    const { data } = await apiClient.post(
+      "/api/v1/email-send/send-test-email",
+      emailData
+    );
+    return data;
+  },
+};
