5dba2ba50d6da34ef0835047a3f85512d2e28171
Curtis Aliwah <aliwahcurtis5@gmail.com>
2025-07-15 09:43:40 +0400

    chore: Update user management table by combining name and email into a user column

--- frontend/src/pages/Admin-Side/User-Management/columns.jsx
+++ frontend/src/pages/Admin-Side/User-Management/columns.jsx
@@ -8,15 +8,41 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { EllipsisIcon, Trash2Icon, User2Icon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export const usersColumns = [
+  //   {
+  //     accessorKey: "name",
+  //     header: "Name",
+  //   },
+  //   {
+  //     accessorKey: "email",
+  //     header: "Email",
+  //   },
   {
-    accessorKey: "name",
-    header: "Name",
-  },
-  {
-    accessorKey: "email",
-    header: "Email",
+    accessorKey: "User",
+    header: "User",
+    cell: ({ row }) => {
+      const { name, email } = row.original;
+      return (
+        <div className="flex items-cennter gap-3">
+          <Avatar className="size-10 rounded-full ">
+            {/* <AvatarImage src={avatar} alt="UserProfile" /> */}
+            <AvatarFallback className="rounded-full bg-fountain-blue-500 text-white">
+              {/* {initials} */}
+              AV
+              {/* <User className="size-5 mx-auto" /> */}
+            </AvatarFallback>
+          </Avatar>
+          <div>
+            <div className="font-medium">{name}</div>
+            <span className="text-muted-foreground mt-0.5 text-xs">
+              {email}
+            </span>
+          </div>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "role",
@@ -25,14 +51,26 @@ export const usersColumns = [
   {
     accessorKey: "ipAddress",
     header: "IP Address",
+    cell: ({ row }) => {
+      const ipAddress = row.getValue("ipAddress");
+      return <div className="text-muted-foreground">{ipAddress}</div>;
+    },
   },
   {
     accessorKey: "lastSignedIn",
     header: "Last Signed In",
+    cell: ({ row }) => {
+      const signedIn = row.getValue("lastSignedIn");
+      return <div className="text-muted-foreground">{signedIn}</div>;
+    },
   },
   {
     accessorKey: "createdAt",
     header: "Created",
+    cell: ({ row }) => {
+      const created = row.getValue("createdAt");
+      return <div className="text-muted-foreground">{created}</div>;
+    },
   },
   {
     id: "actions",
